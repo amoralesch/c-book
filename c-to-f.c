2b3d4cd3ae8a92e2d2c568ac456e89aabc15d5d3
Antonio Morales <amoralesch@hotmail.com>
2021-04-13 22:37:19 -0400

    Simplify temperature examples

--- c-to-f.c
+++ c-to-f.c
@@ -7,20 +7,8 @@
 
 int main()
 {
-  float fahr, celsius;
-  int lower, upper, step;
-
-  lower = 0;    // lower limit of temperature
-  upper = 300;  // upper limit
-  step = 20;    // step size
-
   printf("Celcius Fahr\n");
-  celsius = lower;
-  while (celsius <= upper) {
-    fahr = ((9.0 / 5.0) * celsius) + 32.0;
 
-    printf("%4.0f %7.1f\n", celsius, fahr);
-
-    celsius = celsius + step;
-  }
+  for (int celsius = 0; celsius < 300; celsius += 20)
+    printf("%3d %7.1f\n", celsius, ((9.0 / 5.0) * celsius) + 32.0);
 }
--- f-to-c.c
+++ f-to-c.c
@@ -7,20 +7,8 @@
 
 int main()
 {
-  float fahr, celsius;
-  int lower, upper, step;
-
-  lower = 0;    // lower limit of temperature
-  upper = 300;  // upper limit
-  step = 20;    // step size
-
   printf("Fahr  Celcius\n");
-  fahr = lower;
-  while (fahr <= upper) {
-    celsius = (5.0 / 9.0) * (fahr - 32.0);
 
-    printf("%4.0f %7.1f\n", fahr, celsius);
-
-    fahr = fahr + step;
-  }
+  for (int fahr = 0; fahr <= 300; fahr += 20)
+    printf("%3d %7.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
 }
